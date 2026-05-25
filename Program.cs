c9a9429ec5ff66230e0c875955bd77ff58eb3b73
Mihalag9 <271818180+Mihalag9@users.noreply.github.com>
2026-05-25 13:49:20 +0300

    настройка CORS и статических файлов

--- Program.cs
+++ Program.cs
@@ -10,16 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Разрешить запросы с любых источников (для учебных целей)
+            .AllowAnyMethod()  // Разрешить любые HTTP-методы (GET, POST, ...)
+            .AllowAnyHeader(); // Разрешить любые заголовки
+    });
+});
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseDefaultFiles(); // Ищет файлы по умолчанию (index.html) 
+app.UseStaticFiles(); // Подключает статические файлы 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
